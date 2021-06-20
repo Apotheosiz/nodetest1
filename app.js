4d9ba08f6b04acad6e02046db51140e8b1438019
Apotheosiz <sascha.kasapovski@student.kyh.se>
2021-06-20 15:11:03 +0200

    added restAPI

--- app.js
+++ app.js
@@ -1,27 +1,51 @@
 // import
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 
 // Get static file
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/img', express.static(__dirname + 'public/img'))
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
+
+// Create a function that displays random number
+function randomNumber(max) {    
+    let randObj = {
+        "number": Math.floor(Math.random() * max)
+    };
+    return randObj
+};
+
+// Function that counts the amount of vowels in a word. Source: https://www.programiz.com/javascript/examples/count-vowels
+function countVowel(countallthevowels) {
+    let countVowel = {
+        "word": countallthevowels,
+        "vowels": count = countallthevowels.match(/[aeiouåäö]/gi).length
+    };
+    return countVowel;
+};
 
 // Set Views
-app.set('views', 'public/views')
-app.set('view engine', 'ejs')
+app.set('views', 'public/views');
+app.set('view engine', 'ejs');
 
 app.get('', (req, res) => {
-    res.render('index', { text: 'EJS test' })
-})
+    res.render('index', { text: 'EJS test' });
+});
 
 app.get('/about', (req, res) => {
-    res.render('about', { text: 'About page' })
-})
+    res.render('about', { text: 'About page' });
+});
 
+app.get('/api/random', (req, res) => {
+    return res.send(randomNumber(1024));
+});
+
+app.get('/api/count_vowels/:countallthevowels', (req, res) => {
+    return res.send(countVowel(req.params.word));
+});
 
 // listen on port
-app.listen(port, () => console.info('Listening on port ${port}'))
+app.listen(port, () => console.info('Listening on port ${port}'));
